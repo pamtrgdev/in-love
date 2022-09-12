500d810922f3101383947ce14900b0365e20d2fe
pamtrgdev <112542614+pamtrgdev@users.noreply.github.com>
2022-09-12 16:07:34 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'You';
+const giftUrl = 'https://www.facebook.com/pamthg/';
+const eventName = 'Hey';
+const titleCard = 'Tặng đằng ấy';
+const contentCard = '“Có thể sự xuất hiện của bạn sẽ tuyệt vời như vai trò của bạn trong cuộc sống này vậy <3.”';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
