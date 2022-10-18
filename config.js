151d82c70c64929f104ba2e47b0760eda0e0ef92
pamtrgdev <112542614+pamtrgdev@users.noreply.github.com>
2022-10-18 10:34:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'You';
-const giftUrl = 'https://www.facebook.com/pamthg/';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100086164826429';
 const eventName = 'Hey';
 const titleCard = 'Tặng đằng ấy';
 const contentCard = '“Có thể sự xuất hiện của bạn sẽ tuyệt vời như vai trò của bạn trong cuộc sống này vậy <3.”';
